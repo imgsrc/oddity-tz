138695b8324d00d980b05bf7e6a078b06de2bd75
imgsrc <forifelse@yandex.ru>
2017-04-25 02:20:37 +0400

    Added header-background, heafer-logo, title and navigation-buttons

---
+++ smartgrid.js
@@ -0,0 +1,32 @@
+var smartgrid = require('smart-grid');
+
+smartgrid('./app/sass/precss', {
+  outputStyle: 'sass',
+  columns: 12,
+  offset: '10px',
+  container: {
+    maxWidth: '1260px'
+  },
+  breakPoints: {
+    lg: {
+      width: '1200px',
+      fields: '15px'
+    },
+    md: {
+      width: '992px',
+      fields: "15px"
+    },
+    sm: {
+      width: "768px",
+      fields: "15px"
+    },
+    xs: {
+      width: "576px",
+      fields: "15px"
+    },
+    xxs: {
+      width: "400px",
+      fields: "15px"
+    }
+  }
+});
